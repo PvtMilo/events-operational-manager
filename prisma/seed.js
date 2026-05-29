6f7316ea0750fa86a91b64122daa5084edf06f65
PvtMilo <flip360.id.solo@gmail.com>
2026-05-29 13:46:28 +0700

    Fix seed generated client import

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import bcrypt from "bcryptjs";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/client";
+import { PrismaClient } from "./generated/client.js";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,

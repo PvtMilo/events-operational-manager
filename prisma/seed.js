bc68b2276413e571e5e8c97435adc241988573ba
PvtMilo <flip360.id.solo@gmail.com>
2026-05-29 14:02:57 +0700

    Run seed with tsx

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import bcrypt from "bcryptjs";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/client.js";
+import { PrismaClient } from "./generated/client.ts";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
